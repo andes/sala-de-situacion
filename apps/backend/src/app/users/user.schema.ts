a19c8acf07ec1a66bf8afc864cf80796bef34e2e
Mariano Andres Botta <marianoabotta@gmail.com>
2020-03-23 13:56:30 -0300

    feat(backend): add password functions

--- apps/backend/src/app/users/user.schema.ts
+++ apps/backend/src/app/users/user.schema.ts
@@ -1,16 +1,50 @@
 import * as mongoose from 'mongoose';
 import { AuditPlugin } from '@andes/mongoose-plugin-audit';
+import { environment } from '../../environments/environment.prod';
+const bcrypt = require('bcrypt');
+
+const EMAILRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const UsersSchema = new mongoose.Schema({
   activo: Boolean,
   nombre: String,
   apellido: String,
   documento: String,
-  email: String,
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    match: EMAILRegexp,
+    index: { unique: true }
+  },
   telefono: String,
   password: String,
   permisos: [String]
 });
-UsersSchema.plugin(AuditPlugin);
+
+if (environment.key) {
+  UsersSchema.plugin(AuditPlugin);
+}
+
+UsersSchema.pre('save', async function(next) {
+  const user: any = this;
+  const SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(SALT_FACTOR);
+    const hash = await bcrypt.hash(user.password, salt, null);
+    user.password = hash;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UsersSchema.methods.comparePassword = async function(passwordAttempt) {
+  return await bcrypt.compare(passwordAttempt, this.password);
+};
 
 export const User = mongoose.model('users', UsersSchema, 'users');
--- apps/backend/src/environments/environment.ts
+++ apps/backend/src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   port: parseInt(process.env.PORT) || 3000,
   host: process.env.HOST || '0.0.0.0',
   key: process.env.JWT_KEY || null,
-  mongo_host: process.env.MONGO_HOST || 'mongodb://localhost:27017'
+  mongo_host:
+    process.env.MONGO_HOST || 'mongodb://localhost:27017/sala-situacion'
 };
