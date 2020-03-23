aa6dfe42e3764a1076a91a9440de244b41f95a97
Mariano Andres Botta <marianoabotta@gmail.com>
2020-03-23 11:54:02 -0300

    add angular app

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sala-de-situacion-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontend';
+}
