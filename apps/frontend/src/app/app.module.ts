17cfcd9bf5d02ed7689f676a62f4034cb5ce857d
Mariano Andres Botta <marianoabotta@gmail.com>
2020-03-23 12:49:04 -0300

    feat(frontend): add plex

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
+import { PlexModule, Plex } from '@andes/plex';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppHomeComponent } from './home/home.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppHomeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    PlexModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([{ path: 'home', component: AppHomeComponent }], {
+      initialNavigation: 'enabled'
+    })
   ],
-  providers: [],
+  providers: [Plex],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
