7ca8207045a857d74829a0097300098b9a376b9d
Hugo <hfernandez@neuquen.gov.ar>
2020-03-24 08:50:58 -0300

    feat(backend): Add institution schema and routes.

---
+++ apps/backend/src/app/institution/institution.controller.ts
@@ -0,0 +1,21 @@
+import { MongoQuery, ResourceBase } from '@andes/core';
+import { Institution } from './institution.schema';
+import { authenticate } from '../application';
+
+class InstitutionResource extends ResourceBase {
+    Model = Institution;
+    resourceName = 'institutions';
+    middlewares = [authenticate()];
+    searchFileds = {
+        nombre: MongoQuery.partialString,
+        direccion: MongoQuery.partialString,
+        provincia: MongoQuery.partialString,
+        localidad: MongoQuery.partialString,
+        tipoInstitutcion: MongoQuery.partialString,
+        activo: MongoQuery.equalMatch,
+        search: ['nombre', 'direccion', 'tipoInstitucion', 'provincia', 'localidad']
+    };
+}
+
+export const InstitutionCtr = new InstitutionResource();
+export const InstitutionRouter = InstitutionCtr.makeRoutes();
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,19 +1,15 @@
 require('dotenv').config();
-
 const { Connections } = require('./app/connection');
-
 const { application } = require('./app/application');
 
 Connections.initialize();
 
 const UsersRouter = require('./app/users/user.controller').UsersRouter;
 const AuthRouter = require('./app/auth/auth.routes').AuthRouter;
-// const andes = require('./app/routes/andes').router;
-// const auth = require('./app/routes/auth').router;
+const InstitutionsRouter = require('./app/institution/institution.controller').InstitutionRouter;
 
 application.add({ path: '/api', router: UsersRouter });
 application.add({ path: '/api', router: AuthRouter });
-// application.add({ path: '/api/andes', router: andes });
-// application.add({ path: '/api/auth', router: auth });
+application.add({ path: '/api', router: InstitutionsRouter });
 
 application.start();
