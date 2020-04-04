047d7387d1ce409c4689bbb5f2e479c130147d6a
Mariano Andres Botta <marianoabotta@gmail.com>
2020-04-04 12:10:14 -0300

    feat(events): edicion de tipos de eventos

--- apps/frontend/src/app/events/events.routing.ts
+++ apps/frontend/src/app/events/events.routing.ts
@@ -2,11 +2,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AppEventsCrudComponent } from './components/events-crud/events-crud.component';
 import { EventsListComponent } from './components/events-list/events-list.component';
+import { EventsResolver } from './resolver/events.resolver';
 
 const routes: Routes = [
     { path: '', component: EventsListComponent, pathMatch: 'full' },
-    { path: 'create', component: AppEventsCrudComponent }
-    // { path: ':id', component: AppEventsCrudComponent }
+    { path: 'create', component: AppEventsCrudComponent },
+    {
+        path: ':id',
+        component: AppEventsCrudComponent,
+        resolve: {
+            event: EventsResolver
+        }
+    }
 ];
 
 @NgModule({
