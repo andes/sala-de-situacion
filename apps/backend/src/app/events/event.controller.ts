7d2de36bb26e69d54c295fc1cfba8570bb5a8b17
silviroa <silviroa@gmail.com>
2020-04-20 16:49:07 -0300

    fix(Events): Agrega control de acceso a eventos para administradores

--- apps/backend/src/app/events/event.controller.ts
+++ apps/backend/src/app/events/event.controller.ts
@@ -1,11 +1,30 @@
 import { MongoQuery, ResourceBase } from '@andes/core';
 import { Event } from './event.schema';
-import { authenticate } from '../application';
+import { authenticate, checkPermission } from '../application';
+import { Request, Response } from '@andes/api-tool';
 
 class EventsResource extends ResourceBase {
     Model = Event;
     resourceName = 'events';
     middlewares = [authenticate()];
+    routesAuthorization = {
+        post: (req: Request, res: Response, next) => {
+            const permisoAdmin = checkPermission(req, 'admin:true');
+            if (!permisoAdmin) {
+                return next(403);
+            }
+            return next();
+
+        },
+        search: (req: Request, res: Response, next) => {
+            const permisoAdmin = checkPermission(req, 'admin:true');
+            if (!permisoAdmin) {
+                return next(403);
+            }
+            return next();
+
+        }
+    };
     searchFileds = {
         nombre: MongoQuery.partialString,
         categoria: MongoQuery.partialString,
