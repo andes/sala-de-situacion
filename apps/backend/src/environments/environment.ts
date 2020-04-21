921a69206127d14abc29054697ab614810102e31
Hugo <hfernandez@neuquen.gov.ar>
2020-04-21 18:07:37 -0300

    fix:(backend): Agregamos el host de app (#52)

--- apps/backend/src/environments/environment.ts
+++ apps/backend/src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: false,
     port: parseInt(process.env.PORT) || 3000,
     host: process.env.HOST || '0.0.0.0',
+    app_host: process.env.APP_HOST || 'https://salasituacion.andes.gob.ar',
     google_map_key: process.env.GOOGLE_KEY || 'AIzaSyC__of8PZKirB_IvkjzI7XTlfYtLieGRh0',
     key: process.env.JWT_KEY || 'jpfg1oayB2p7iaTAh7s790GuSsgal/mEF7A8jpnqpQQ=',
     mongo_host: process.env.MONGO_HOST || 'mongodb://localhost:27017/sala-situacion',
