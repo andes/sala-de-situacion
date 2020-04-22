bea0791673ba1d159cf9416e73da580ca8467cbe
Juliana <juli.otero01@gmail.com>
2020-04-22 15:31:19 -0300

    feat(perfil-usuario): carga de datos del usuario logueado y las instituciones asociadas

--- apps/backend/src/app/auth/auth.routes.ts
+++ apps/backend/src/app/auth/auth.routes.ts
@@ -8,84 +8,84 @@ import { environment } from '../../environments/environment';
 export const AuthRouter = application.router();
 
 AuthRouter.post('/auth/login', async (req: Request, res, next) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-        const users = await UsersCtr.search({ email: email, active: true }, {}, req);
-        if (users.length > 0) {
-            const user = users[0];
-            const match = await user.comparePassword(password);
-            if (match) {
-                const session = new Types.ObjectId();
-                const newToken = await application.sign({
-                    session_id: session.toHexString(),
-                    user_id: user._id.toHexString()
-                });
-                return res.json({ token: newToken });
-            }
-        }
-        return next(403);
-    } catch (err) {
-        return next(403);
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const users = await UsersCtr.search({ email: email, active: true }, {}, req);
+    if (users.length > 0) {
+      const user = users[0];
+      const match = await user.comparePassword(password);
+      if (match) {
+        const session = new Types.ObjectId();
+        const newToken = await application.sign({
+          session_id: session.toHexString(),
+          user_id: user._id.toHexString()
+        });
+        return res.json({ token: newToken });
+      }
     }
+    return next(403);
+  } catch (err) {
+    return next(403);
+  }
 });
 
 AuthRouter.post('/auth/create', async (req: Request, res, next) => {
-    try {
-        const user = req.body;
-        const createdUser = await UsersCtr.create(user, req);
-        const url = `${environment.app_host}/auth/activacion-cuenta/${createdUser.validationToken}`;
-        await sendEmailNotification(user.email, user.nombre, 'SALA DE SITUACIÓN :: Verificación de cuenta', `${user.nombre}, gracias por registrar tu cuenta. Para activarla haz click aquí ${url}`);
-        return res.json({ status: 'ok' });
-    } catch (err) {
-        return next(403);
-    }
+  try {
+    const user = req.body;
+    const createdUser = await UsersCtr.create(user, req);
+    const url = `${environment.app_host}/auth/activacion-cuenta/${createdUser.validationToken}`;
+    await sendEmailNotification(user.email, user.nombre, 'SALA DE SITUACIÓN :: Verificación de cuenta', `${user.nombre}, gracias por registrar tu cuenta. Para activarla haz click aquí ${url}`);
+    return res.json({ status: 'ok' });
+  } catch (err) {
+    return next(403);
+  }
 });
 
 AuthRouter.post('/auth/regenerate/:email', async (req: Request, res, next) => {
-    try {
-        const email = req.params.email;
-        const updatedUser = await UsersCtr.setNewToken(email, req);
-        const url = `${environment.app_host}/auth/regenerate-password/${updatedUser.validationToken}`;
-        await sendEmailNotification(email, updatedUser.nombre, 'SALA DE SITUACIÓN :: Regenerar contraseña', `Hola ${updatedUser.nombre}, para regenerar la contraseña de tu cuenta por favor hacer clic aquí: ${url}`);
-        return res.json({ status: 'ok' });
-    } catch (err) {
-        return next(403);
-    }
+  try {
+    const email = req.params.email;
+    const updatedUser = await UsersCtr.setNewToken(email, req);
+    const url = `${environment.app_host}/auth/regenerate-password/${updatedUser.validationToken}`;
+    await sendEmailNotification(email, updatedUser.nombre, 'SALA DE SITUACIÓN :: Regenerar contraseña', `Hola ${updatedUser.nombre}, para regenerar la contraseña de tu cuenta por favor hacer clic aquí: ${url}`);
+    return res.json({ status: 'ok' });
+  } catch (err) {
+    return next(403);
+  }
 });
 
 AuthRouter.post('/auth/validate/:token', async (req: Request, res, next) => {
-    try {
-        const token = req.params.token;
-        await UsersCtr.validateUser(token, req);
-        return res.json({ status: 'ok' });
-    } catch (err) {
-        return next(403);
-    }
+  try {
+    const token = req.params.token;
+    await UsersCtr.validateUser(token, req);
+    return res.json({ status: 'ok' });
+  } catch (err) {
+    return next(403);
+  }
 });
 
 AuthRouter.post('/auth/resetPassword', async (req: Request, res, next) => {
-    try {
-        const { password, validationToken } = req.body;
-        const users = await UsersCtr.search({ token: validationToken, active: true }, {}, req);
-        if (users.length > 0) {
-            await UsersCtr.update(users[0].id, { password, validationToken: null }, req);
-            return res.json({ status: 'ok' });
-        } else {
-            return res.json({ status: 404 });
-        }
-    } catch (err) {
-        return next(403);
+  try {
+    const { password, validationToken } = req.body;
+    const users = await UsersCtr.search({ token: validationToken, active: true }, {}, req);
+    if (users.length > 0) {
+      await UsersCtr.update(users[0].id, { password, validationToken: null }, req);
+      return res.json({ status: 'ok' });
+    } else {
+      return res.json({ status: 404 });
     }
+  } catch (err) {
+    return next(403);
+  }
 });
 
 AuthRouter.get('/auth/session', application.authenticate(), async (req, res, next) => {
-    try {
-        const user_id = req.user.user_id;
-        const user = await UsersCtr.findById(user_id, { fields: 'nombre apellido permisos active' });
-        res.json(user);
+  try {
+    const user_id = req.user.user_id;
+    const user = await UsersCtr.findById(user_id, { fields: 'nombre apellido telefono email documento permisos active' });
+    res.json(user);
 
-    } catch (err) {
-        return next(403);
-    }
+  } catch (err) {
+    return next(403);
+  }
 });
--- apps/frontend/src/app/login/components/perfil/user-profile.component.ts
+++ apps/frontend/src/app/login/components/perfil/user-profile.component.ts
@@ -1,34 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../../login/auth.services';
+import { InstitutionService } from '../../../institutions/service/institution.service';
+import { Plex } from '@andes/plex';
 
 
 @Component({
-    selector: 'user-profile',
-    templateUrl: './user-profile.html',
-    styleUrls: ['./user-profile.scss']
+  selector: 'user-profile',
+  templateUrl: './user-profile.html',
+  styleUrls: ['./user-profile.scss']
 })
 export class UserProfileComponent implements OnInit {
 
-    institutions: any = [{ "activo": true, "_id": "5e97532aca549149abbba241", "nombre": "CLINICA PASTEUR", "email": "pasteur@pasteur.com", "telefono": "2990000000", "direccion": "av siempreviva 123", "localidad": "Neuquén", "provincia": "Neuquén", "users": [{ "permisos": ["covid19:indicators:write"], "_id": "5e887b38294bcf12ae6ef93d", "documento": "29410428", "nombre": "Carolina", "apellido": "Celeste" }], "createdAt": "2020-04-15T18:32:10.724Z", "createdBy": { "email": "celeste.carolina.s@gmail.com", "nombre": "Carolina", "apellido": "Celeste", "documento": "29410428" }, "barrio": "", "updatedAt": "2020-04-20T16:53:15.113Z", "updatedBy": { "email": "celeste.carolina.s@gmail.com", "nombre": "Carolina", "apellido": "Celeste", "documento": "29410428", "organizacion": null }, "coordenadas": [-38.9642578, -68.0445886], "id": "5e97532aca549149abbba241" }, { "activo": true, "_id": "5e97beef7a4d190b21fddc20", "nombre": "CLINICA ADOS", "email": "ados@ados.com", "telefono": "", "direccion": "Tucumán 436", "localidad": "Neuquén", "provincia": "Neuquén", "users": [{ "permisos": [], "_id": "5e9c8d89dc9448497aea4721", "documento": "30093263", "nombre": "Silvina", "apellido": "Roa" }], "createdAt": "2020-04-16T02:11:59.705Z", "createdBy": { "email": "celeste.carolina.s@gmail.com", "nombre": "Carolina", "apellido": "Celeste", "documento": "29410428" }, "barrio": "", "updatedAt": "2020-04-18T12:35:57.055Z", "updatedBy": { "email": "andresin@gmail.com", "nombre": "ANDRÉS", "apellido": "BOC-HO", "documento": "29148200", "organizacion": null }, "coordenadas": [-38.9504124, -68.0521847], "id": "5e97beef7a4d190b21fddc20" }];
+  public institutions = [];
+  public user;
+  showModalResetPassword = false;
+  showUsarios = [];
+  constructor(
+    public plex: Plex,
+    private auth: AuthService,
+    private institutionService: InstitutionService
+  ) { }
 
-    showModalResetPassword = false;
-    showUsarios = [];
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router
-    ) { }
+  usuario: any = {};
+  token = '';
 
-    usuario: any = {};
-    token = '';
+  ngOnInit(): void {
+    this.auth.getSession().subscribe((sessionUser) => {
+      this.user = sessionUser;
+    });
+    let paramsInstitutions: any = {};
+    paramsInstitutions.user = this.user.id;
+    this.institutionService.search(paramsInstitutions).subscribe(
+      registros => {
+        this.institutions = registros;
+      },
+      (err) => {
+      }
+    );
+  }
 
-    ngOnInit(): void { }
+  reset() {
+    this.showModalResetPassword = true;
+  }
 
-    reset() {
-        this.showModalResetPassword = true;
-    }
+  close(event) {
+    this.showModalResetPassword = false;
+  }
 
-    close(event) {
-        this.showModalResetPassword = false;
-    }
+  addUserToInstitution(institution, user) {
+    institution.users.push(user);
+    this.institutionService.save(institution).subscribe(rta => {
+      this.plex.toast('success', `El usuario fue agregado correctamente a la institución ${rta.nombre}.`);
+    });
+  }
 }
