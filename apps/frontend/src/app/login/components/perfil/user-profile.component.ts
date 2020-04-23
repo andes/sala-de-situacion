f543686d98a0616a96590129020e51a3ad7b78f8
Andrés Boc-Ho <andresin@gmail.com>
2020-04-23 11:45:10 -0300

    feat(user-profile): modal reset password

--- apps/frontend/src/app/login/components/perfil/user-profile.component.ts
+++ apps/frontend/src/app/login/components/perfil/user-profile.component.ts
@@ -6,88 +6,88 @@ import { Plex } from '@andes/plex';
 
 
 @Component({
-  selector: 'user-profile',
-  templateUrl: './user-profile.html',
-  styleUrls: ['./user-profile.scss']
+    selector: 'user-profile',
+    templateUrl: './user-profile.html',
+    styleUrls: ['./user-profile.scss']
 })
 export class UserProfileComponent implements OnInit {
 
-  public institutions = [];
-  public users: any[];
-  public candidateUsers = [];
-  public user;
-  showResetPassword = false;
-  showUsarios = [];
+    public institutions = [];
+    public users: any[];
+    public candidateUsers = [];
+    public user;
+    showModalResetPassword = false;
+    showUsarios = [];
 
-  public loading = false;
-  public password1 = '';
-  public password2 = '';
-  public passwordActual = '';
-  public token;
-  constructor(
-    public plex: Plex,
-    public auth: AuthService,
-    private institutionService: InstitutionService,
-    private userService: UserService
-  ) { }
+    public loading = false;
+    public password1 = '';
+    public password2 = '';
+    public passwordActual = '';
+    public token;
+    constructor(
+        public plex: Plex,
+        public auth: AuthService,
+        private institutionService: InstitutionService,
+        private userService: UserService
+    ) { }
 
-  usuario: any = {};
-  selectedUser: any = {};
+    usuario: any = {};
+    selectedUser: any = {};
 
-  ngOnInit(): void {
-    this.auth.getSession().subscribe((sessionUser) => {
-      this.user = sessionUser;
-    });
-    let paramsInstitutions: any = {};
-    paramsInstitutions.user = this.user.id;
-    this.institutionService.search(paramsInstitutions).subscribe(
-      registros => {
-        this.institutions = registros;
-        this.userService.search().subscribe(
-          registros => {
-            this.users = registros;
-            this.institutions.forEach((inst) => {
-              const institutionUsers = inst.users.map((u) => u.documento);
-              let candidates: any[] = this.users.filter(item => institutionUsers.indexOf(item.documento) < 0);
-              this.candidateUsers[inst.id] = {
-                users: candidates
-              };
-            });
-          },
-          (err) => {
-          }
+    ngOnInit(): void {
+        this.auth.getSession().subscribe((sessionUser) => {
+            this.user = sessionUser;
+        });
+        let paramsInstitutions: any = {};
+        paramsInstitutions.user = this.user.id;
+        this.institutionService.search(paramsInstitutions).subscribe(
+            registros => {
+                this.institutions = registros;
+                this.userService.search().subscribe(
+                    registros => {
+                        this.users = registros;
+                        this.institutions.forEach((inst) => {
+                            const institutionUsers = inst.users.map((u) => u.documento);
+                            let candidates: any[] = this.users.filter(item => institutionUsers.indexOf(item.documento) < 0);
+                            this.candidateUsers[inst.id] = {
+                                users: candidates
+                            };
+                        });
+                    },
+                    (err) => {
+                    }
+                );
+            },
+            (err) => {
+            }
         );
-      },
-      (err) => {
-      }
-    );
-    console.log(this.candidateUsers);
-  }
+        console.log(this.candidateUsers);
+    }
 
-  showResetForm() {
-    this.showResetPassword = !this.showResetPassword;
-  }
+    toggleResetForm() {
+        this.showModalResetPassword = !this.showModalResetPassword;
+    }
 
-  addUserToInstitution(institution) {
-    institution.users.push(this.selectedUser);
-    this.institutionService.save(institution).subscribe(rta => {
-      this.plex.toast('success', `El usuario fue agregado correctamente a la institución ${rta.nombre}.`);
-      var index = this.candidateUsers[institution.id].users.indexOf(this.selectedUser);
-      this.candidateUsers[institution.id].users.splice(index, 1);
-      this.selectedUser = {};
-    });
-  }
+    addUserToInstitution(institution) {
+        institution.users.push(this.selectedUser);
+        this.institutionService.save(institution).subscribe(rta => {
+            this.plex.toast('success', `El usuario fue agregado correctamente a la institución ${rta.nombre}.`);
+            var index = this.candidateUsers[institution.id].users.indexOf(this.selectedUser);
+            this.candidateUsers[institution.id].users.splice(index, 1);
+            this.selectedUser = {};
+        });
+    }
 
-  deleteUserFromInstitution(institution, user) {
-    var index = institution.users.indexOf(user);
-    institution.users.splice(index, 1);
-    this.institutionService.save(institution).subscribe(rta => {
-      this.plex.toast('success', `El usuario fue desvinculado correctamente de la institución ${rta.nombre}.`);
-      this.candidateUsers[institution.id].users.push(user);
-    });
-  }
+    deleteUserFromInstitution(institution, user) {
+        var index = institution.users.indexOf(user);
+        institution.users.splice(index, 1);
+        this.institutionService.save(institution).subscribe(rta => {
+            this.plex.toast('success', `El usuario fue desvinculado correctamente de la institución ${rta.nombre}.`);
+            this.candidateUsers[institution.id].users.push(user);
+        });
+    }
 
-  save(password) {
+    save(password) {
 
-  }
+    }
 }
