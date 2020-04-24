6367812537708da149b39da20b8e0bc662a755c9
Juliana <juli.otero01@gmail.com>
2020-04-24 12:35:26 -0300

    refactor: control al sumar usuarios a instituciones

--- apps/backend/src/app/users/user.controller.ts
+++ apps/backend/src/app/users/user.controller.ts
@@ -5,38 +5,38 @@ import { authenticate } from '../application';
 import * as mongoose from 'mongoose';
 
 class UsersResource extends ResourceBase {
-    Model = User;
-    resourceName = 'users';
-    resourceModule = 'auth';
-    middlewares = [authenticate()];
-    searchFileds = {
-        apellido: MongoQuery.partialString,
-        nombre: MongoQuery.partialString,
-        documento: MongoQuery.partialString,
-        validationToken: MongoQuery.equalMatch,
-        active: MongoQuery.equalMatch,
-        email: MongoQuery.equalMatch,
-        search: ['apellido', 'nombre', 'documento', 'email'],
-    };
+  Model = User;
+  resourceName = 'users';
+  resourceModule = 'auth';
+  middlewares = [authenticate()];
+  searchFileds = {
+    apellido: MongoQuery.partialString,
+    nombre: MongoQuery.partialString,
+    documento: MongoQuery.partialString,
+    validationToken: MongoQuery.equalMatch,
+    active: MongoQuery.equalMatch,
+    email: MongoQuery.partialString,
+    search: ['apellido', 'nombre', 'documento', 'email'],
+  };
 
-    async validateUser(token: string, req: Request) {
-        const users = await this.search({ validationToken: token, active: false }, {}, req);
-        if (users.length > 0) {
-            const user = users[0];
-            const data = { validationToken: null, active: true };
-            return await this.update(user._id, data, req);
-        }
-        throw new ResourceNotFound();
+  async validateUser(token: string, req: Request) {
+    const users = await this.search({ validationToken: token, active: false }, {}, req);
+    if (users.length > 0) {
+      const user = users[0];
+      const data = { validationToken: null, active: true };
+      return await this.update(user._id, data, req);
     }
+    throw new ResourceNotFound();
+  }
 
-    async setNewToken(email: string, req: Request) {
-        const users = await this.search({ email, active: true }, {}, req);
-        if (users.length > 0) {
-            const user = users[0];
-            const data = { validationToken: new mongoose.Types.ObjectId().toHexString() };
-            return await this.update(user._id, data, req);
-        }
+  async setNewToken(email: string, req: Request) {
+    const users = await this.search({ email, active: true }, {}, req);
+    if (users.length > 0) {
+      const user = users[0];
+      const data = { validationToken: new mongoose.Types.ObjectId().toHexString() };
+      return await this.update(user._id, data, req);
     }
+  }
 }
 
 export const UsersCtr = new UsersResource();
--- apps/frontend/src/app/login/components/perfil/user-profile.component.ts
+++ apps/frontend/src/app/login/components/perfil/user-profile.component.ts
@@ -6,123 +6,110 @@ import { Plex } from '@andes/plex';
 
 
 @Component({
-    selector: 'user-profile',
-    templateUrl: './user-profile.html',
-    styleUrls: ['./user-profile.scss']
+  selector: 'user-profile',
+  templateUrl: './user-profile.html',
+  styleUrls: ['./user-profile.scss']
 })
 export class UserProfileComponent implements OnInit {
 
-    public institutions = [];
-    public users: any[];
-    public candidateUsers = [];
-    public user;
-    showModalResetPassword = false;
-    showUsarios = [];
+  public institutions = [];
+  public users: any[];
+  public candidateUsers = [];
+  public user;
+  showModalResetPassword = false;
+  showUsarios = [];
 
-    public loading = false;
-    public password1 = '';
-    public password2 = '';
-    public passwordActual = '';
-    public token;
-    constructor(
-        public plex: Plex,
-        public auth: AuthService,
-        private institutionService: InstitutionService,
-        private userService: UserService
-    ) { }
+  public loading = false;
+  public password1 = '';
+  public password2 = '';
+  public passwordActual = '';
+  public token;
+  constructor(
+    public plex: Plex,
+    public auth: AuthService,
+    private institutionService: InstitutionService,
+    private userService: UserService
+  ) { }
 
-    usuario: any = {};
-    selectedUser: any = {};
+  usuario: any = {};
+  selectedUser: any = {};
 
-    ngOnInit(): void {
-        this.auth.getSession().subscribe((sessionUser) => {
-            this.user = sessionUser;
-        });
-        let paramsInstitutions: any = {};
-        paramsInstitutions.user = this.user.id;
-        this.institutionService.search(paramsInstitutions).subscribe(
-            registros => {
-                this.institutions = registros;
-                this.userService.search().subscribe(
-                    registros => {
-                        this.users = registros;
-                        this.institutions.forEach((inst) => {
-                            const institutionUsers = inst.users.map((u) => u.documento);
-                            let candidates: any[] = this.users.filter(item => institutionUsers.indexOf(item.documento) < 0);
-                            this.candidateUsers[inst.id] = {
-                                // users: candidates
-                                users: candidates.map(ux => ({
-                                    _id: ux.id,
-                                    id: ux.id,
-                                    nombre: ux.nombre,
-                                    apellido: ux.apellido,
-                                    permisos: ux.permisos,
-                                    documento: ux.documento,
-                                    email: ux.email
-                                }))
-                            };
-                        });
-                    },
-                    (err) => {
-                    }
-                );
-            },
-            (err) => {
-            }
+  ngOnInit(): void {
+    this.auth.getSession().subscribe((sessionUser) => {
+      this.user = sessionUser;
+    });
+    let paramsInstitutions: any = {};
+    paramsInstitutions.user = this.user.id;
+    this.institutionService.search(paramsInstitutions).subscribe(
+      registros => {
+        this.institutions = registros;
+        this.userService.search().subscribe(
+          registros => {
+            this.users = registros;
+          },
+          (err) => {
+          }
         );
-        console.log(this.candidateUsers);
-    }
+      },
+      (err) => {
+      }
+    );
+    console.log(this.candidateUsers);
+  }
 
-    toggleResetForm() {
-        this.showModalResetPassword = !this.showModalResetPassword;
-    }
+  toggleResetForm() {
+    this.showModalResetPassword = !this.showModalResetPassword;
+  }
 
-    toggleShowUsuarios(i) {
-        this.showUsarios[i] = !this.showUsarios[i];
-    }
+  toggleShowUsuarios(i) {
+    this.showUsarios[i] = !this.showUsarios[i];
+  }
 
-    refreshCandidates(id) {
-        this.candidateUsers[id].users = [...this.candidateUsers[id].users];
-    }
+  refreshCandidates(id) {
+    this.candidateUsers[id].users = [...this.candidateUsers[id].users];
+  }
 
-    loadUsers(event, institution) {
-        if (event.query) {
-            let query = {
-                email: event.query
-            };
-
-            this.userService.search
-                (query).subscribe(resultado => {
-                    event.callback(resultado);
-                });
-        } else {
-            event.callback([this.selectedUser] || []);
-        }
-    }
-
-    addUserToInstitution(institution) {
-        // institution.users.push(this.selectedUser);
-        institution.users = [...institution.users, this.selectedUser];
-        this.institutionService.save(institution).subscribe(rta => {
-            this.plex.toast('success', `El usuario fue agregado correctamente a la institución ${rta.nombre}.`);
-            var index = this.candidateUsers[institution.id].users.indexOf(this.selectedUser);
-            this.candidateUsers[institution.id].users.splice(index, 1);
-            this.selectedUser = {};
-            this.refreshCandidates(institution.id);
+  loadUsers(event) {
+    if (event.query) {
+      let query = {
+        search: "^" + event.query
+      };
+      this.userService.search
+        (query).subscribe(resultado => {
+          event.callback(resultado);
         });
+    } else {
+      event.callback([this.selectedUser] || []);
     }
+  }
 
-    deleteUserFromInstitution(institution, user) {
-        var index = institution.users.indexOf(user);
-        institution.users.splice(index, 1);
-        this.institutionService.save(institution).subscribe(rta => {
-            this.plex.toast('success', `El usuario fue desvinculado correctamente de la institución ${rta.nombre}.`);
-            this.candidateUsers[institution.id].users = [...this.candidateUsers[institution.id].users, user];
-            this.refreshCandidates(institution.id);
-        });
+  addUserToInstitution(institution) {
+    let existeUsuario = institution.users.filter(item => item.id === this.selectedUser.id).length > 0;
+    if (existeUsuario) {
+      this.plex.toast('danger', `El usuario ya se encuenta asociado a la institución.`);
+    } else {
+      institution.users.push(this.selectedUser);
+      this.institutionService.save(institution).subscribe(rta => {
+        this.plex.toast('success', `El usuario fue agregado correctamente a la institución ${rta.nombre}.`);
+        var index = this.candidateUsers[institution.id].users.indexOf(this.selectedUser);
+        this.candidateUsers[institution.id].users.splice(index, 1);
+        this.selectedUser = {};
+        this.refreshCandidates(institution.id);
+      });
     }
+  }
 
-    save(password) {
+  deleteUserFromInstitution(institution, user) {
+    var index = institution.users.indexOf(user);
+    institution.users.splice(index, 1);
+    this.institutionService.save(institution).subscribe(rta => {
+      this.plex.toast('success', `El usuario fue desvinculado correctamente de la institución ${rta.nombre}.`);
+      this.candidateUsers[institution.id].users = [...this.candidateUsers[institution.id].users, user];
+      this.refreshCandidates(institution.id);
+    });
+  }
 
-    }
+  save(password) {
+
+  }
 }
