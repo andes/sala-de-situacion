ced2c133d1c493be938dcd75dacfee2d70fda915
Hugo <hfernandez@neuquen.gov.ar>
2020-04-24 08:31:31 -0300

    fix(backend): add google keys field (#55)

--- apps/backend/src/environments/environment.prod.ts
+++ apps/backend/src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   port: parseInt(process.env.PORT) || 3000,
   host: process.env.HOST || '0.0.0.0',
+  google_map_key: process.env.GOOGLE_KEY || 'unacalve',
   app_host: process.env.APP_HOST || 'https://salasituacion.andes.gob.ar',
   key: process.env.JWT_KEY || null,
   mongo_host: process.env.MONGO_HOST || 'mongodb://localhost:27017/sala-situacion',
--- apps/backend/src/environments/environment.ts
+++ apps/backend/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     port: parseInt(process.env.PORT) || 3000,
     host: process.env.HOST || '0.0.0.0',
     app_host: process.env.APP_HOST || 'https://salasituacion.andes.gob.ar',
-    google_map_key: process.env.GOOGLE_KEY || 'AIzaSyC__of8PZKirB_IvkjzI7XTlfYtLieGRh0',
+    google_map_key: process.env.GOOGLE_KEY || 'unacalve',
     key: process.env.JWT_KEY || 'jpfg1oayB2p7iaTAh7s790GuSsgal/mEF7A8jpnqpQQ=',
     mongo_host: process.env.MONGO_HOST || 'mongodb://localhost:27017/sala-situacion',
     mail: {
