60bea86737bad588750b02f78cdbc83042ea06fd
Juliana <juli.otero01@gmail.com>
2020-04-24 13:54:31 -0300

    refactor(perfil-usuario): limpieza de código y corrección en modal al aceptar/cancelar

--- apps/frontend/src/app/login/components/perfil/user-profile.component.ts
+++ apps/frontend/src/app/login/components/perfil/user-profile.component.ts
@@ -6,127 +6,99 @@ import { Plex } from '@andes/plex';
 
 
 @Component({
-    selector: 'user-profile',
-    templateUrl: './user-profile.html',
-    styleUrls: ['./user-profile.scss']
+  selector: 'user-profile',
+  templateUrl: './user-profile.html',
+  styleUrls: ['./user-profile.scss']
 })
 export class UserProfileComponent implements OnInit {
 
-    // Current user
-    public user;
+  // Current user
+  public user;
+  // Institutions
+  public institutions = [];
+  public showUsarios = [];
+  // User Model
+  public selectedUser: any = {};
+  // Reset password
+  public showModalResetPassword = false;
+  public loading = false;
+  public password1 = '';
+  public password2 = '';
+  public passwordActual = '';
+  public token;
+  // Sugerencias
+  public showModalSugerencias = false;
+  public sugerencias: any = {};
 
-    // Institutions
-    public institutions = [];
-    public showUsarios = [];
+  constructor(
+    public plex: Plex,
+    public auth: AuthService,
+    private institutionService: InstitutionService,
+    private userService: UserService
+  ) { }
 
-    // Users
-    public users: any[];
-    public candidateUsers = [];
+  ngOnInit(): void {
+    this.auth.getSession().subscribe((sessionUser) => {
+      this.user = sessionUser;
+    });
+    let paramsInstitutions: any = {};
+    paramsInstitutions.user = this.user.id;
+    this.institutionService.search(paramsInstitutions).subscribe(
+      registros => {
+        this.institutions = registros;
+      },
+      (err) => {
+      }
+    );
+  }
 
-    // User Model
-    public selectedUser: any = {};
+  toggleResetForm() {
+    this.showModalResetPassword = !this.showModalResetPassword;
+  }
 
-    // Reset password
-    public showModalResetPassword = false;
-    public loading = false;
-    public password1 = '';
-    public password2 = '';
-    public passwordActual = '';
-    public token;
+  toggleShowUsuarios(i) {
+    this.showUsarios[i] = !this.showUsarios[i];
+  }
 
-    // Sugerencias
-    public showModalSugerencias = false;
-    // Sugerencias Model
-    public sugerencias: any = {};
+  toggleShowSugerencias() {
+    this.showModalSugerencias = !this.showModalSugerencias;
+  }
 
-    constructor(
-        public plex: Plex,
-        public auth: AuthService,
-        private institutionService: InstitutionService,
-        private userService: UserService
-    ) { }
-
-
-
-    ngOnInit(): void {
-        this.auth.getSession().subscribe((sessionUser) => {
-            this.user = sessionUser;
+  loadUsers(event) {
+    if (event.query) {
+      let query = {
+        search: "^" + event.query
+      };
+      this.userService.search
+        (query).subscribe(resultado => {
+          event.callback(resultado);
         });
-        let paramsInstitutions: any = {};
-        paramsInstitutions.user = this.user.id;
-        this.institutionService.search(paramsInstitutions).subscribe(
-            registros => {
-                this.institutions = registros;
-                this.userService.search().subscribe(
-                    registros => {
-                        this.users = registros;
-                    },
-                    (err) => {
-                    }
-                );
-            },
-            (err) => {
-            }
-        );
-        console.log(this.candidateUsers);
+    } else {
+      event.callback([this.selectedUser] || []);
     }
+  }
 
-    toggleResetForm() {
-        this.showModalResetPassword = !this.showModalResetPassword;
+  addUserToInstitution(institution) {
+    console.log(institution.users);
+    let existeUsuario = institution.users.filter(item => item.id === this.selectedUser.id).length > 0;
+    console.log(this.selectedUser.id);
+    if (existeUsuario) {
+      this.plex.toast('danger', `El usuario ya se encuenta asociado a la institución.`);
+    } else {
+      institution.users.push(this.selectedUser);
+      this.institutionService.save(institution).subscribe(rta => {
+        this.plex.toast('success', `El usuario fue agregado correctamente a la institución ${rta.nombre}.`);
+        this.selectedUser = {};
+      });
     }
+  }
 
-    toggleShowUsuarios(i) {
-        this.showUsarios[i] = !this.showUsarios[i];
-    }
-    toggleShowSugerencias() {
-        this.showModalSugerencias = !this.showModalSugerencias;
-    }
+  deleteUserFromInstitution(institution, user) {
+    var index = institution.users.indexOf(user);
+    institution.users.splice(index, 1);
+    this.institutionService.save(institution).subscribe(rta => {
+      this.plex.toast('success', `El usuario fue desvinculado correctamente de la institución ${rta.nombre}.`);
+    });
+  }
 
-    refreshCandidates(id) {
-        this.candidateUsers[id].users = [...this.candidateUsers[id].users];
-    }
-
-    loadUsers(event) {
-        if (event.query) {
-            let query = {
-                search: "^" + event.query
-            };
-            this.userService.search
-                (query).subscribe(resultado => {
-                    event.callback(resultado);
-                });
-        } else {
-            event.callback([this.selectedUser] || []);
-        }
-    }
-
-    addUserToInstitution(institution) {
-        let existeUsuario = institution.users.filter(item => item.id === this.selectedUser.id).length > 0;
-        if (existeUsuario) {
-            this.plex.toast('danger', `El usuario ya se encuenta asociado a la institución.`);
-        } else {
-            institution.users.push(this.selectedUser);
-            this.institutionService.save(institution).subscribe(rta => {
-                this.plex.toast('success', `El usuario fue agregado correctamente a la institución ${rta.nombre}.`);
-                var index = this.candidateUsers[institution.id].users.indexOf(this.selectedUser);
-                this.candidateUsers[institution.id].users.splice(index, 1);
-                this.selectedUser = {};
-                this.refreshCandidates(institution.id);
-            });
-        }
-    }
-
-    deleteUserFromInstitution(institution, user) {
-        var index = institution.users.indexOf(user);
-        institution.users.splice(index, 1);
-        this.institutionService.save(institution).subscribe(rta => {
-            this.plex.toast('success', `El usuario fue desvinculado correctamente de la institución ${rta.nombre}.`);
-            this.candidateUsers[institution.id].users = [...this.candidateUsers[institution.id].users, user];
-            this.refreshCandidates(institution.id);
-        });
-    }
-
-    save(password) {
-
-    }
 }
--- apps/frontend/src/app/login/components/regenerate-password/regenerate-password.ts
+++ apps/frontend/src/app/login/components/regenerate-password/regenerate-password.ts
@@ -8,69 +8,78 @@ import { Observable } from 'rxjs';
 import { PlexModalComponent } from '@andes/plex/src/lib/modal/modal.component';
 
 @Component({
-    selector: 'modal-regenerate-password',
-    templateUrl: 'regenerate-password.html',
-    styleUrls: ['regenerate-password.scss'],
+  selector: 'modal-regenerate-password',
+  templateUrl: 'regenerate-password.html',
+  styleUrls: ['regenerate-password.scss'],
 })
 export class RegeneratePasswordComponent implements OnInit {
 
-    @ViewChild('modal', { static: true }) modal: PlexModalComponent;
+  @ViewChild('modal', { static: true }) modal: PlexModalComponent;
 
-    @Input()
-    set show(value) {
-        if (value) {
-            this.modal.show();
-        }
+  @Input()
+  set show(value) {
+    if (value) {
+      this.modal.show();
     }
+  }
 
-    @Output() closeModal = new EventEmitter<any>();
+  @Output() closeModal = new EventEmitter<any>();
 
-    public loading = false;
-    public password1 = '';
-    public password2 = '';
-    public user$: Observable<IUsuario>;
-    public token;
+  public loading = false;
+  public password1 = '';
+  public password2 = '';
+  public user$: Observable<IUsuario>;
+  public token;
 
-    constructor(private plex: Plex, private route: ActivatedRoute, public auth: AuthService, private router: Router) { }
+  constructor(private plex: Plex, private route: ActivatedRoute, public auth: AuthService, private router: Router) { }
 
-    ngOnInit() {
-        //Busca el token y activa la cuenta
-        this.route.paramMap.subscribe(params => {
-            this.token = params.get('token');
-            if (this.token) {
-                this.modal.show();
-            }
-        });
-    }
+  ngOnInit() {
+    //Busca el token y activa la cuenta
+    this.route.paramMap.subscribe(params => {
+      this.token = params.get('token');
+      if (this.token) {
+        this.modal.show();
+      }
+    });
+  }
 
-    save(event) {
-        if (event.formValid) {
-            if (this.password1 === this.password2) {
-                this.loading = true;
-                this.auth.resetPassword({ token: this.token, password: this.password1 }).subscribe(
-                    data => {
-                        this.plex.info('success', 'La contraseña ha sido restablecida correctamente');
-                        this.loading = false;
-                    },
-                    err => {
-                        this.plex.info('danger', err);
-                        this.loading = false;
-                    }
-                );
-            } else {
-                this.plex.info('danger', 'Las contraseñas no coinciden', 'Error contraseñas');
-            }
-
-        }
-    }
-
-    cancel() {
+  save(event) {
+    if (event.formValid) {
+      if (this.password1 === this.password2) {
+        this.loading = true;
+        this.auth.resetPassword({ token: this.token, password: this.password1 }).subscribe(
+          data => {
+            this.plex.toast('success', 'La contraseña ha sido restablecida correctamente');
+            this.clearForm();
+            this.modal.close();
+          },
+          err => {
+            this.plex.info('danger', err);
+            this.loading = false;
+          }
+        );
+      } else {
+        this.plex.info('danger', 'Las contraseñas no coinciden', 'Error contraseñas');
         this.modal.showed = false;
         this.auth.showPassword = false;
         this.closeModal.emit();
-        if (this.token) {
-            this.router.navigate(['/auth/login']);
-        }
+      }
     }
+  }
+
+  cancel() {
+    this.modal.showed = false;
+    this.auth.showPassword = false;
+    this.closeModal.emit();
+    if (this.token) {
+      this.router.navigate(['/auth/login']);
+    }
+  }
+
+  clearForm() {
+    this.loading = false;
+    this.password1 = '';
+    this.password2 = '';
+  }
 
 }
