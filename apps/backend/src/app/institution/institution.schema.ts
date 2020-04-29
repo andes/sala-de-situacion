8eb2081448bdd0c0755a568949effc9eef0bb6ec
Juliana <juli.otero01@gmail.com>
2020-04-29 11:35:48 -0300

    feat(intituciones): sumado campo opcional sisa a instituciones

--- apps/backend/src/app/institution/institution.schema.ts
+++ apps/backend/src/app/institution/institution.schema.ts
@@ -3,38 +3,41 @@ import { environment } from '../../environments/environment';
 import { AuditPlugin } from '@andes/mongoose-plugin-audit';
 
 export const InstitutionsSchema = new mongoose.Schema({
-  activo: { type: Boolean, default: false },
-  nombre: String,
-  email: String,
-  telefono: String,
-  coordenadas: {
-    type: [Number],
-    index: '2d'
-  },
-  direccion: String,
-  barrio: String,
-  localidad: String,
-  provincia: String,
-  zona: String,
-  tipoInstitutcion: String,
-  referente: {
+    activo: { type: Boolean, default: false },
     nombre: String,
-    apellido: String,
-    telefono: String
-  },
-  users: [
-    {
-      id: String,
-      nombre: String,
-      apellido: String,
-      documento: String,
-      permisos: [String]
-    }
-  ]
+    email: String,
+    telefono: String,
+    coordenadas: {
+        type: [Number],
+        index: '2d'
+    },
+    direccion: String,
+    barrio: String,
+    localidad: String,
+    provincia: String,
+    zona: String,
+    tipoInstitutcion: String,
+    codigo: {
+        sisa: String
+    },
+    referente: {
+        nombre: String,
+        apellido: String,
+        telefono: String
+    },
+    users: [
+        {
+            id: String,
+            nombre: String,
+            apellido: String,
+            documento: String,
+            permisos: [String]
+        }
+    ]
 });
 
 if (environment.key) {
-  InstitutionsSchema.plugin(AuditPlugin);
+    InstitutionsSchema.plugin(AuditPlugin);
 }
 
 export const Institution = mongoose.model('institutions', InstitutionsSchema, 'institutions');
--- apps/frontend/src/app/institutions/components/institutions-crud/institution-crud.component.ts
+++ apps/frontend/src/app/institutions/components/institutions-crud/institution-crud.component.ts
@@ -8,207 +8,214 @@ import { GeoreferenciaService } from '../../service/georeferencia.service';
 import { Utils } from './../../../shared/utils';
 
 @Component({
-  selector: 'institution-crud',
-  templateUrl: './institution-crud.component.html'
+    selector: 'institution-crud',
+    templateUrl: './institution-crud.component.html'
 })
 export class AppInstitutionCrudComponent implements OnInit {
-  provNeuquen = {
-    _id: '5e80b38065773d7db8dc6015',
-    nombre: 'Neuquén'
-    //id: '5e80b38065773d7db8dc6015'
-  };
-  public institution = {
-    id: '',
-    nombre: '',
-    email: '',
-    telefono: '',
-    location: {
-      provincia: null,
-      localidad: null,
-      barrio: null,
-      direccion: '',
-      coordenadas: []
-    },
-    referente: {
-      nombre: '',
-      apellido: '',
-      telefono: ''
-    },
-    activo: false,
-    users: []
-  };
-  neuquen = true;
-  provincias = [];
-  localidades = [];
-  barrios = [];
-  institutionParam = null;
-  geoReferenciaAux = []; // Coordenadas para la vista del mapa.
-  infoMarcador: String = null;
-  public isAdmin;
-  public disableGuardar = false;
+    provNeuquen = {
+        _id: '5e80b38065773d7db8dc6015',
+        nombre: 'Neuquén'
+        //id: '5e80b38065773d7db8dc6015'
+    };
+    public institution = {
+        id: '',
+        nombre: '',
+        email: '',
+        telefono: '',
+        location: {
+            provincia: null,
+            localidad: null,
+            barrio: null,
+            direccion: '',
+            coordenadas: []
+        },
+        codigo: {
+            sisa: ''
+        },
+        referente: {
+            nombre: '',
+            apellido: '',
+            telefono: ''
+        },
+        activo: false,
+        users: []
+    };
+    neuquen = true;
+    provincias = [];
+    localidades = [];
+    barrios = [];
+    institutionParam = null;
+    geoReferenciaAux = []; // Coordenadas para la vista del mapa.
+    infoMarcador: String = null;
+    public isAdmin;
+    public disableGuardar = false;
 
-  constructor(
-    public route: ActivatedRoute, // Permite obtener objetos o datos por parámetro.
-    public plex: Plex,
-    private locationService: LocationService,
-    private institutionService: InstitutionService,
-    private router: Router,
-    private auth: AuthService,
-    private georeferenciaService: GeoreferenciaService
-  ) { }
+    constructor(
+        public route: ActivatedRoute, // Permite obtener objetos o datos por parámetro.
+        public plex: Plex,
+        private locationService: LocationService,
+        private institutionService: InstitutionService,
+        private router: Router,
+        private auth: AuthService,
+        private georeferenciaService: GeoreferenciaService
+    ) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this.isAdmin = this.auth.checkPermisos('admin:true');
-    this.institutionParam = this.route.snapshot.params; // Si viene un id es un update
-    if (this.institutionParam.id) {
-      this.institutionService.get(this.institutionParam.id).subscribe(rta => {
-        this.loadInstitution(rta);
-        this.inicializarMapa();
-        this.neuquen = this.institutionParam.provincia === 'Neuquén' ? true : false;
-        this.locationService.getLocalidades({ provincia: this.institution.location.provincia.id }).subscribe(rta => {
-          this.localidades = rta;
-        });
-      });
-    } else {
-      this.institution.location.provincia = this.provNeuquen;
-      this.locationService.getLocalidades({ provincia: this.institution.location.provincia.id }).subscribe(rta => {
-        this.localidades = rta;
-      });
-    }
-    this.locationService.getProvincias({}).subscribe(rta => {
-      this.provincias = rta;
-    });
-  }
-
-  loadInstitution(institucion) {
-    this.institution.id = institucion.id;
-    this.institution.nombre = institucion.nombre;
-    this.institution.email = institucion.email;
-    this.institution.telefono = institucion.telefono;
-    this.institution.referente.nombre = institucion.referente ? institucion.referente.nombre : '';
-    this.institution.referente.apellido = institucion.referente ? institucion.referente.apellido : '';
-    this.institution.referente.telefono = institucion.referente ? institucion.referente.telefono : '';
-    this.institution.location.direccion = institucion.direccion ? institucion.direccion : '';
-    this.institution.location.barrio = institucion.barrio
-      ? this.locationService.getBarrios({ nombre: institucion.barrio }).subscribe(barrios => {
-        this.institution.location.barrio = barrios[0];
-      })
-      : '';
-    this.institution.location.localidad = institucion.localidad
-      ? this.locationService.getLocalidades({ nombre: institucion.localidad }).subscribe(localidades => {
-        this.institution.location.localidad = localidades[0];
-      })
-      : '';
-    this.institution.location.provincia = institucion.provincia
-      ? this.locationService.getProvincias({ nombre: institucion.provincia }).subscribe(provincias => {
-        this.institution.location.provincia = provincias[0];
-      })
-      : '';
-    this.institution.activo = institucion.activo;
-    this.institution.location.coordenadas = institucion.coordenadas ? JSON.parse("[" + institucion.coordenadas + "]") : []; //Convierte a array las corrdenadas
-  }
-
-  setNeuquen(event) {
-    if (event.value) {
-      this.institution.location.provincia = this.provNeuquen;
-      this.locationService
-        .getLocalidades({ provincia: this.institution.location.provincia.id })
-        .subscribe(rta => {
-          this.localidades = rta;
-        });
-    }
-  }
-
-  changeProvincia(event) {
-    this.institution.location.localidad = {};
-    this.institution.location.barrio = {};
-    if (this.institution.location.provincia) {
-      this.locationService.getLocalidades({ provincia: this.institution.location.provincia.id }).subscribe(rta => {
-        this.localidades = rta;
-      });
-    }
-  }
-
-  changeLocalidad(event) {
-    this.institution.location.barrio = {};
-    if (this.institution.location.localidad) {
-      this.locationService.getBarrios({ localidad: this.institution.location.localidad.id }).subscribe(rta => {
-        this.barrios = rta;
-      });
-    }
-  }
-
-  guardar($event) {
-    if ($event.formValid) {
-      try {
-        let dto = {
-          id: this.institution.id,
-          nombre: this.institution.nombre,
-          email: this.institution.email,
-          telefono: this.institution.telefono,
-          referente: {
-            nombre: this.institution.referente.nombre,
-            apellido: this.institution.referente.apellido,
-            telefono: this.institution.referente.telefono
-          },
-          direccion: this.institution.location.direccion,
-          barrio: this.institution.location.barrio.nombre ? this.institution.location.barrio.nombre : '',
-          localidad: this.institution.location.localidad.nombre,
-          provincia: this.institution.location.provincia.nombre,
-          activo: this.institution.activo,
-          coordenadas: []
-        };
-        if (this.institution.location.provincia && this.institution.location.localidad && this.institution.location.direccion) {
-          let direccionCompleta = this.institution.location.direccion + ', ' + this.institution.location.localidad.nombre
-            + ', ' + this.institution.location.provincia.nombre;
-          // se calcula la georeferencia
-          this.georeferenciaService.get({ direccion: direccionCompleta }).subscribe(point => {
-            if (point && point.lat && point.lng) {
-              this.institution.location.coordenadas = [point.lat, point.lng];
-              dto.coordenadas = [point.lat, point.lng];
-            }
-            this.guardarInstitucion(dto);
-          });
+        this.isAdmin = this.auth.checkPermisos('admin:true');
+        this.institutionParam = this.route.snapshot.params; // Si viene un id es un update
+        if (this.institutionParam.id) {
+            this.institutionService.get(this.institutionParam.id).subscribe(rta => {
+                this.loadInstitution(rta);
+                this.inicializarMapa();
+                this.neuquen = this.institutionParam.provincia === 'Neuquén' ? true : false;
+                this.locationService.getLocalidades({ provincia: this.institution.location.provincia.id }).subscribe(rta => {
+                    this.localidades = rta;
+                });
+            });
         } else {
-          this.guardarInstitucion(dto);
+            this.institution.location.provincia = this.provNeuquen;
+            this.locationService.getLocalidades({ provincia: this.institution.location.provincia.id }).subscribe(rta => {
+                this.localidades = rta;
+            });
         }
-      }
-      catch (err) {
-        this.plex.info('danger', 'Error al guardar la institución');
-      }
+        this.locationService.getProvincias({}).subscribe(rta => {
+            this.provincias = rta;
+        });
     }
-  }
 
-  guardarInstitucion(institucion) {
-    this.institutionService.save(institucion).subscribe(rta => {
-      this.plex.toast('success', `La institución ${rta.nombre} se guardó correctamente`);
-      this.mainInsitutions();
-    });
-  }
-
-  cancelar() {
-    this.mainInsitutions();
-  }
-
-  mainInsitutions() {
-    this.router.navigate(['/institution/list']);
-  }
-
-  verificarFormatoEmail() {
-    let utils = new Utils();
-    this.disableGuardar = !utils.verificarFormatoEmail(this.institution.email);
-  }
-
-  inicializarMapa() {
-    if (this.institution.location.coordenadas) {
-      this.geoReferenciaAux = this.institution.location.coordenadas;
+    loadInstitution(institucion) {
+        this.institution.id = institucion.id;
+        this.institution.nombre = institucion.nombre;
+        this.institution.email = institucion.email;
+        this.institution.telefono = institucion.telefono;
+        this.institution.codigo.sisa = institucion.codigo ? institucion.codigo.sisa : '';;
+        this.institution.referente.nombre = institucion.referente ? institucion.referente.nombre : '';
+        this.institution.referente.apellido = institucion.referente ? institucion.referente.apellido : '';
+        this.institution.referente.telefono = institucion.referente ? institucion.referente.telefono : '';
+        this.institution.location.direccion = institucion.direccion ? institucion.direccion : '';
+        this.institution.location.barrio = institucion.barrio
+            ? this.locationService.getBarrios({ nombre: institucion.barrio }).subscribe(barrios => {
+                this.institution.location.barrio = barrios[0];
+            })
+            : '';
+        this.institution.location.localidad = institucion.localidad
+            ? this.locationService.getLocalidades({ nombre: institucion.localidad }).subscribe(localidades => {
+                this.institution.location.localidad = localidades[0];
+            })
+            : '';
+        this.institution.location.provincia = institucion.provincia
+            ? this.locationService.getProvincias({ nombre: institucion.provincia }).subscribe(provincias => {
+                this.institution.location.provincia = provincias[0];
+            })
+            : '';
+        this.institution.activo = institucion.activo;
+        this.institution.location.coordenadas = institucion.coordenadas ? JSON.parse("[" + institucion.coordenadas + "]") : []; //Convierte a array las corrdenadas
     }
-  }
 
-  changeCoordenadas(coordenadas) {
-    this.geoReferenciaAux = coordenadas;    // Se actualiza vista del mapa
-    this.institution.location.coordenadas = coordenadas;    // Se asigna nueva georeferencia al paciente
-  }
+    setNeuquen(event) {
+        if (event.value) {
+            this.institution.location.provincia = this.provNeuquen;
+            this.locationService
+                .getLocalidades({ provincia: this.institution.location.provincia.id })
+                .subscribe(rta => {
+                    this.localidades = rta;
+                });
+        }
+    }
+
+    changeProvincia(event) {
+        this.institution.location.localidad = {};
+        this.institution.location.barrio = {};
+        if (this.institution.location.provincia) {
+            this.locationService.getLocalidades({ provincia: this.institution.location.provincia.id }).subscribe(rta => {
+                this.localidades = rta;
+            });
+        }
+    }
+
+    changeLocalidad(event) {
+        this.institution.location.barrio = {};
+        if (this.institution.location.localidad) {
+            this.locationService.getBarrios({ localidad: this.institution.location.localidad.id }).subscribe(rta => {
+                this.barrios = rta;
+            });
+        }
+    }
+
+    guardar($event) {
+        if ($event.formValid) {
+            try {
+                let dto = {
+                    id: this.institution.id,
+                    nombre: this.institution.nombre,
+                    email: this.institution.email,
+                    telefono: this.institution.telefono,
+                    codigo: {
+                        sisa: this.institution.codigo.sisa
+                    },
+                    referente: {
+                        nombre: this.institution.referente.nombre,
+                        apellido: this.institution.referente.apellido,
+                        telefono: this.institution.referente.telefono
+                    },
+                    direccion: this.institution.location.direccion,
+                    barrio: this.institution.location.barrio.nombre ? this.institution.location.barrio.nombre : '',
+                    localidad: this.institution.location.localidad.nombre,
+                    provincia: this.institution.location.provincia.nombre,
+                    activo: this.institution.activo,
+                    coordenadas: []
+                };
+                if (this.institution.location.provincia && this.institution.location.localidad && this.institution.location.direccion) {
+                    let direccionCompleta = this.institution.location.direccion + ', ' + this.institution.location.localidad.nombre
+                        + ', ' + this.institution.location.provincia.nombre;
+                    // se calcula la georeferencia
+                    this.georeferenciaService.get({ direccion: direccionCompleta }).subscribe(point => {
+                        if (point && point.lat && point.lng) {
+                            this.institution.location.coordenadas = [point.lat, point.lng];
+                            dto.coordenadas = [point.lat, point.lng];
+                        }
+                        this.guardarInstitucion(dto);
+                    });
+                } else {
+                    this.guardarInstitucion(dto);
+                }
+            }
+            catch (err) {
+                this.plex.info('danger', 'Error al guardar la institución');
+            }
+        }
+    }
+
+    guardarInstitucion(institucion) {
+        this.institutionService.save(institucion).subscribe(rta => {
+            this.plex.toast('success', `La institución ${rta.nombre} se guardó correctamente`);
+            this.mainInsitutions();
+        });
+    }
+
+    cancelar() {
+        this.mainInsitutions();
+    }
+
+    mainInsitutions() {
+        this.router.navigate(['/institution/list']);
+    }
+
+    verificarFormatoEmail() {
+        let utils = new Utils();
+        this.disableGuardar = !utils.verificarFormatoEmail(this.institution.email);
+    }
+
+    inicializarMapa() {
+        if (this.institution.location.coordenadas) {
+            this.geoReferenciaAux = this.institution.location.coordenadas;
+        }
+    }
+
+    changeCoordenadas(coordenadas) {
+        this.geoReferenciaAux = coordenadas;    // Se actualiza vista del mapa
+        this.institution.location.coordenadas = coordenadas;    // Se asigna nueva georeferencia al paciente
+    }
 
 }
