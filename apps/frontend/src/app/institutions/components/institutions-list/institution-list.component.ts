814f072535d2ce445b34052efeeb5b7f32cfdd2c
juliotero <juli.otero01@gmail.com>
2020-05-02 12:41:22 -0300

    INSTITUCIONES: sumados filtros al listado de instituciones (#59)
    * feat(instituciones): sumados filtros al listado de instituciones
    
    * fix(intitutions): Quitamos el request adicional y dejamos solo el cache.
    
    * fix(instituciones): fix del listado de instituciones
    
    Co-authored-by: Hugo <hfernandez@neuquen.gov.ar>

--- apps/frontend/src/app/institutions/components/institutions-list/institution-list.component.ts
+++ apps/frontend/src/app/institutions/components/institutions-list/institution-list.component.ts
@@ -3,14 +3,24 @@ import { Plex } from '@andes/plex';
 import { LocationService } from '../../../shared/location.services';
 import { InstitutionService } from '../../service/institution.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { cache } from '@andes/shared';
 
 @Component({
     selector: 'institutionListComponent',
     templateUrl: './institution-list.component.html'
 })
 export class AppInstitutionListComponent implements OnInit {
+    public institutions$: Observable<any[]>;
     public institutions = [];
     public institucion = null;
+    public nombre = null;
+    public estados: any[];
+    public estado = null;
+    public localidad = null;
+    public localidades: any[] = [];
+    public provincia = null;
+    public provincias: any[] = [];
     public activateInstitution = false;
     constructor(
         public plex: Plex,
@@ -20,9 +30,12 @@ export class AppInstitutionListComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.institutionService.search({}).subscribe(rta => {
-            this.institutions = rta;
+        this.institutions$ = this.institutionService.search().pipe(cache());
+        this.locationService.getProvincias({}).subscribe(rta => {
+            this.provincias = rta;
         });
+        this.estados = [{ id: 'true', nombre: 'Habilitada' }, { id: 'false', nombre: 'No habilitada' }];
+        this.filtrarResultados();
     }
 
     edit(institution) {
@@ -43,11 +56,40 @@ export class AppInstitutionListComponent implements OnInit {
         });
     }
 
+    changeProvincia(event) {
+        if (this.provincia) {
+            this.locationService.getLocalidades({ provincia: this.provincia.id }).subscribe(rta => {
+                this.localidades = rta;
+            });
+        } else {
+            this.localidades = [];
+        }
+        this.filtrarResultados();
+    }
+
     create() {
         this.router.navigate(['/institution/crud']);
     }
 
     mainMenu() {
         this.router.navigate(['/']);
+    }
+
+    filtrarResultados() {
+        this.institutions$.subscribe(value => {
+            this.institutions = value;
+            if (this.nombre) {
+                this.institutions = this.institutions.filter(e => e.nombre.toLowerCase().search(this.nombre.toLowerCase()) != -1);
+            }
+            if (this.estado) {
+                this.institutions = this.institutions.filter(e => e.activo.toString() === this.estado.id);
+            }
+            if (this.provincia) {
+                this.institutions = this.institutions.filter(e => e.provincia === this.provincia.nombre);
+                if (this.localidad) {
+                    this.institutions = this.institutions.filter(e => e.localidad === this.localidad.nombre);
+                }
+            }
+        });
     }
 }
