47f12e79ed426576a963aa0b67492d4108de9ef2
Juliana <juli.otero01@gmail.com>
2020-05-18 11:27:15 -0300

    fix: rebase fix

--- apps/backend/src/app/institution/institution.schema.ts
+++ apps/backend/src/app/institution/institution.schema.ts
@@ -22,48 +22,29 @@ export const InstitutionsSchema = new mongoose.Schema({
     },
     referente: {
         nombre: String,
-        email: String,
-        telefono: String,
-        coordenadas: {
-            type: [Number],
-            index: '2d'
-        },
-        direccion: String,
-        barrio: String,
-        localidad: String,
-        provincia: String,
-        zona: String,
-        tipoInstitutcion: String,
-        codigo: {
-            sisa: String
-        },
-        referente: {
+        apellido: String,
+        telefono: String
+    },
+    institutions: [
+        {
+            id: String,
+            nombre: String
+        }
+    ],
+    users: [
+        {
+            id: String,
             nombre: String,
             apellido: String,
             telefono: String
-        },
-        representante: {
-            nombre: String,
-            apellido: String,
-            telefono: String
-        },
-        institutions: [
-            {
-                id: String,
-                nombre: String
-            }
-        ],
-        users: [
-            {
-                id: String,
-                nombre: String,
-                apellido: String,
-                documento: String,
-                email: String,
-                permisos: [String]
-            }
-        ]
-    });
+        }
+    ],
+    representante: {
+        nombre: String,
+        apellido: String,
+        telefono: String
+    },
+});
 
 if (environment.key) {
     InstitutionsSchema.plugin(AuditPlugin);
