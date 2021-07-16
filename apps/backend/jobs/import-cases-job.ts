498fd4eb66d92076a946f15c5b291c20926f0043
condorpiedra <natalia.huenchuman@gmail.com>
2021-07-16 14:51:52 -0300

    fix(import): cambia inicio del offset del import de casos (#131)

--- apps/backend/jobs/import-cases-job.ts
+++ apps/backend/jobs/import-cases-job.ts
@@ -1,10 +1,7 @@
 import { importCasesCovid } from '../src/app/covid-events/covid-events';
-import { CovidEventsTemp } from '../src/app/covid-events/covid-events.schema';
 
 async function run(done) {
     await importCasesCovid(done);
-    await CovidEventsTemp.db.dropCollection('covid_events');
-    await CovidEventsTemp.db.collection('covid_events_temp').rename('covid_events');
 }
 
 export default run;
